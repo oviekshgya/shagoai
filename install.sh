256bf30d06ad145fd8109b6d11cd7b934790f5e9
oviekshgya <oviekshgy@gmail.com>
2026-06-04 16:25:54 +0700

    Add

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 set -e
 
 APP_NAME="shagoai"
-REPO_URL="${SHAGO_REPO_URL:-git+https://github.com/oviekshgya/ollama-agent.git}"
+REPO_URL="${SHAGO_REPO_URL:-git+https://github.com/oviekshgya/shagoai.git}"
 
 CONFIG_DIR="$HOME/.config/shagoai"
 CONFIG_FILE="$CONFIG_DIR/config.json"
